979313abf2c8ff5ca8974c23011733b2ef3ed6c4
praveen <praveenryali058@gmail.com>
2023-08-30 12:01:20 +0530

    first commit

---
+++ Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment13.Models
+{
+    [Table("Team")]
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
